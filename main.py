9416918cb573e21538a492c764548dcabfb92dec
Mathieu VACANCE <mathieu.vacance@epitech.eu>
2022-06-08 12:38:42 +0200

    Mathieu VACANCE |  add cors headers

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ import requests
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:5000"
+    "http://localhost:3000",
+    "http://localhost",
+    "http://192.168.0.114:3000",
+    "http://192.168.0.114"
 ] 
 
 app.add_middleware(
